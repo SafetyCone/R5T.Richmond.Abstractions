a90efca160a51be6e076570aab0265edc6152ccc
MinexAutomation <minex.automation@gmail.com>
2020-01-16 10:55:46 -0800

    Moved the IStartup interface type from R5T.Richmond into this abstractions library.

--- source/R5T.Richmond.Abstractions/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Richmond.Abstractions
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Richmond.Abstractions/Code/Interfaces/IStartup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Richmond
+{
+    /// <summary>
+    /// Describes a startup type that configures a configuration builder, a services collection, and a service provider.
+    /// </summary>
+    public interface IStartup
+    {
+        void ConfigureConfiguration(IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider);
+        void ConfigureServices(IServiceCollection services);
+        void Configure(IServiceProvider serviceProvider);
+    }
+}
